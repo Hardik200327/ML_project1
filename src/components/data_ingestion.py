160bc942f22526b5883ff90e28a58e7375336c1f
“[firstname <choudharyhardik491@gmail.com>
2025-07-22 21:03:29 +0530

    new input

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+ 
+from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str = os.path.join("artifacts", "train.csv")
+    test_data_path: str = os.path.join("artifacts", "test.csv")
+    raw_data_path: str = os.path.join("artifacts", "data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered Data Ingestion method")
+        try:
+            df = pd.read_csv("notebook/data/stud.csv")
+            logging.info('Read data set')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Ingestion of data is completed") # Corrected a typo here ("loging")
+
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e, sys)
+
+# THIS BLOCK IS NOW CORRECTLY PLACED AND CALLED
+if __name__ == "__main__":
+    obj = DataIngestion()
+    # Storing the returned paths in a variable
+    train_path, test_path = obj.initiate_data_ingestion() 
+    
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_path,test_path)
+            
+
+
+
+ 
+
