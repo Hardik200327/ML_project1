efbb3c291d07f149d546f8c4dbda114fef464a59
“[firstname <choudharyhardik491@gmail.com>
2025-07-23 10:04:01 +0530

    completed flask

--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import sys
 
 from src.exception import CustomException
 from sklearn.metrics import r2_score 
+from sklearn.model_selection import GridSearchCV
 
 
 
@@ -18,12 +19,19 @@ def save_objects(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)   
 
-def evaluate_models(X_train, y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
            
 
             
@@ -43,7 +51,11 @@ def evaluate_models(X_train, y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
 
         return report
-
     except Exception as e:
         raise CustomException(e, sys)
+def load_object(file_path):
+    with open(file_path,"rb") as file_obj:
+        return dill.load(file_obj)
+
+ 
     
